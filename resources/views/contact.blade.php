e18468f6bcbae9156997122aa1577e84376b6a4d
‪ABDULRAHMAN THABIT‬‏ <bfhmy0529@gmail.com>
2026-06-12 01:15:39 +0300

    Create contact.blade.php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact - LaravelProject</title>
+</head>
+<body>
+    <h1>Contact</h1>
+    <p>Developer: ABDULRAHMAN THABIT</p>
+    <p>GitHub: bfhmy0529-art</p>
+</body>
+</html>
