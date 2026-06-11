aec2f471b81db30e69c103b99acd6d3df3380208
‪ABDULRAHMAN THABIT‬‏ <bfhmy0529@gmail.com>
2026-06-11 21:54:21 +0300

    Create welcome.blade.php

---
+++ docs/routes/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LaravelProject</title>
+</head>
+<body>
+    <h1>Welcome to LaravelProject</h1>
+    <p>Developed by ABDULRAHMAN THABIT</p>
+</body>
+</html>
